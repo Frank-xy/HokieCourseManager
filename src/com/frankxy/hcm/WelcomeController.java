d916549ec1be4bc4544576661c14ea7456ea6c65
Frank-xy <1029655671@qq.com>
2020-07-29 20:03:21 -0500

    Add drag functions

--- src/com/frankxy/hcm/WelcomeController.java
+++ src/com/frankxy/hcm/WelcomeController.java
@@ -6,13 +6,41 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class WelcomeController {
+    /**
+     * The original X position
+     */
+    private double xOffset = 0;
+    /**
+     * The original Y position
+     */
+    private double yOffset = 0;
     @FXML
     private Pane welcomepane;
     @FXML
+    private Pane dragpane;
+    @FXML
     private JFXButton exitbutton;
 
+    /**
+     * exitConfirm
+     */
     public void exitConfirm() {
         Stage stage = (Stage) exitbutton.getScene().getWindow();
         stage.close();
+    }
+
+    /**
+     * dragStage
+     */
+    public void dragStage() {
+        Stage stage = (Stage) dragpane.getScene().getWindow();
+        dragpane.setOnMousePressed(mouseEvent -> {
+            xOffset = mouseEvent.getSceneX();
+            yOffset = mouseEvent.getSceneY();
+        });
+        stage.getScene().setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX() - xOffset);
+            stage.setY(mouseEvent.getScreenY() - yOffset);
+        });
     }
 }
