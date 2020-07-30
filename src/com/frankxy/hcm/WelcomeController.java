87e4ee82e4394d6b7ed1f27fccfcaaa13b51e421
Frank-xy <1029655671@qq.com>
2020-07-30 15:41:34 -0500

    Add a lot of functions

--- src/com/frankxy/hcm/WelcomeController.java
+++ src/com/frankxy/hcm/WelcomeController.java
@@ -3,28 +3,14 @@ package com.frankxy.hcm;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.effect.GaussianBlur;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
 public class WelcomeController {
     /**
-     * The original X position
+     * The original X and Y position
      */
-    private double xOffset = 0;
-    /**
-     * The original Y position
-     */
-    private double yOffset = 0;
-    @FXML
-    private AnchorPane welcomepane;
-    @FXML
-    private ImageView welcomebackground;
+    private double xOffset, yOffset = 0;
     @FXML
     private Pane dragpane;
     @FXML
@@ -56,4 +42,4 @@ public class WelcomeController {
             stage.setY(mouseEvent.getScreenY() - yOffset);
         });
     }
-}
+    }
